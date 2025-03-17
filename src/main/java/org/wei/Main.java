738c524f2d7410ed6e330b4bc7bda979d852ad97
wanghewei <deanwanghewei@163.com>
2025-03-17 11:57:28 +0800

    Initial commit

---
+++ src/main/java/org/wei/Main.java
@@ -0,0 +1,12 @@
+package org.wei;
+
+/**
+ * @author deanwanghewei@gmail.com
+ * @description
+ * @date 2025年03月17日 11:54
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
